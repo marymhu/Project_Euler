c21f5be48084e2abd45453555ce82787f5ffc6dd
Maryline Huet <huet@systerel.fr>
2019-01-31 16:14:25 +0100

    First Problem solved

---
+++ euler_01.py
@@ -0,0 +1,14 @@
+'''
+Multiples of 3 and 5
+https://projecteuler.net/problem=1
+'''
+
+def multiples():
+    mult_list = []
+    for i in range (1000):
+        if i%3 == 0 or i%5 == 0:
+            mult_list.append(i)
+    return sum(mult_list)
+    
+if __name__ == '__main__':
+    print (multiples())
