eb90f11f330ec0b03a0f53e8405adbe1ec5c3e06
marymhu <huet_mary@yahoo.fr>
2019-02-05 16:49:55 +0100

    Refactored using the Sieve of Erathosthenes... still too long

--- euler_10.py
+++ euler_10.py
@@ -6,25 +6,20 @@ https://projecteuler.net/problem=10
 
 def compute_prime_sum(max_val):
     """
-    Compute the sum of the prime numbers below max_val
+    Compute the sum of the prime numbers below max_val using the Sieve of Eratosthenes
     """
-    prime_list = [2, 3]
-    cur_val = prime_list[-1]
-    while cur_val < max_val:
-        cur_val += 2
-        is_prime = True
-        for p in prime_list:
-            if cur_val % p == 0:
-                is_prime = False
-                break
-        if is_prime and cur_val < max_val:
-            prime_list.append(cur_val)
-    return sum(prime_list)
+    candidates = [x for x in range(2, max_val) if x <= 3 or x % 2 != 0 or x % 3 != 0]
+    sum_prime = 0
+    while candidates:
+        current = candidates[0]
+        sum_prime += current
+        candidates = [x for x in candidates if x > current and x % current != 0]
+    return sum_prime
 
 
 if __name__ == '__main__':
     # Provided test case
     assert 17 == compute_prime_sum(10)
-    
+
     # Get Result
     print(compute_prime_sum(2000000))
