1a895fef43b8a5e0ad96ccd5891a6933212c1479
marymhu <huet_mary@yahoo.fr>
2019-02-07 14:02:07 +0100

    Update with best divisors search. Exec time < 5s

--- euler_12.py
+++ euler_12.py
@@ -2,7 +2,7 @@
 Highly divisible triangular number
 https://projecteuler.net/problem=12
 """
-
+from time import time
 
 def generate_next_triangle(rank, val):
     """
@@ -17,10 +17,18 @@ def find_divisible_triangle(divisor_number):
     """
     current_rank = 1
     current_triangle_value = 1
-    divisors = []
-    while len(divisors) < divisor_number - 1:
+    divisors = 0
+    while divisors < divisor_number - 1:
         current_rank, current_triangle_value = generate_next_triangle(current_rank, current_triangle_value)
-        divisors = [num for num in range(1, current_triangle_value) if current_triangle_value % num == 0]
+        divisors = 0
+        # Divisors going by pair (p*q or q*p), we only look for divisors from 1 to value
+        for div in range (2, int(current_triangle_value ** 0.5) + 1):
+            if current_triangle_value % div == 0:
+                # if div is sqrt(value), only one divisors is found
+                if (div * div == current_triangle_value):
+                    divisors += 1
+                else:
+                    divisors += 2
     return current_triangle_value
 
 
@@ -29,4 +37,6 @@ if __name__ == '__main__':
     assert 28 == find_divisible_triangle(5),\
         "Error while computing the first triangle number to have over 5 divisors"
     # Get Result
-    find_divisible_triangle(500)
+    start = time()
+    print(find_divisible_triangle(500))
+    print("Exectution time: " + str(time() - start))
