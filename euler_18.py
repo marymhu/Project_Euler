528fc839a09021ebc60f1b61ddeffff20d85e6ec
marymhu <huet_mary@yahoo.fr>
2019-02-15 16:15:21 +0100

    Resolve by adding max from bottom to top

---
+++ euler_18.py
@@ -0,0 +1,37 @@
+"""
+Maximum path sum I
+https://projecteuler.net/problem=18
+"""
+from time import time
+
+
+def max_sum_path (input_data):
+    length = int(len(input_data) ** 0.5)
+    # Sum maximum value from bottom to top
+    for j in range(length - 2, -1, -1):
+        for i in range(j + 1):
+            input_data[i + j * length] += max(input_data[i + (j + 1) * length],\
+                                              input_data[ i + 1 + (j + 1) * length])
+    return input_data[0]
+
+
+if __name__ == '__main__':
+    input_data = [75, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                  95, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                  17, 47, 82, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                  18, 35, 87, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                  20, 4, 82, 47, 65, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                  19, 1, 23, 75, 3, 34, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                  88, 2, 77, 73, 7, 63, 67, 0, 0, 0, 0, 0, 0, 0, 0,
+                  99, 65, 4, 28, 6, 16, 70, 92, 0, 0, 0, 0, 0, 0, 0,
+                  41, 41, 26, 56, 83, 40, 80, 70, 33, 0, 0, 0, 0, 0, 0,
+                  41, 48, 72, 33, 47, 32, 37, 16, 94, 29, 0, 0, 0, 0, 0,
+                  53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14, 0, 0, 0, 0,
+                  70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57, 0, 0, 0,
+                  91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48, 0, 0,
+                  63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31, 0,
+                  4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23]
+    # Compute result
+    start = time()
+    print(max_sum_path (input_data))
+    print("Execution time: " + str(time() - start))
